e4ddbc92a2e1d47c8df0e60c84b3d2ba7bb972d5
Jay Merlan <jr.merlan@gmail.com>
2018-09-29 00:18:47 -0700

    Added solution for local C# WPF app

---
+++ App/Wpf/Wpf/SharedParameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf
+{
+    class SharedParameter
+    {
+        public string Name { get; set; }
+        public Guid Guid { get; set; }
+        public string DataType { get; set; }
+        public int NodeId { get; set; }
+    }
+}
